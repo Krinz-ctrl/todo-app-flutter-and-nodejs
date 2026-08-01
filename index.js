641f1bc2b63833e4fb4e313a9ba5ccd18783adaf
Dev Kansakar <dknsakr@gmail.com>
2026-08-01 14:00:41 +0545

    app and index and nodeom

---
+++ index.js
@@ -0,0 +1,11 @@
+const app = require('./app');
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("helloworld oki muzi");
+})
+
+app.listen(port, () => {
+    console.log(`server is running on port http://localhost:${port}`);
+});
